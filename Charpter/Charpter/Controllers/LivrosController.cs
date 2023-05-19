d044d759a3feab6a6e584b9c1b93a2ecd0fe642e
LucasKJR <lucaskalencastro@gmail.com>
2023-05-19 13:26:31 -0300

    uc14

---
+++ Charpter/Charpter/Controllers/LivrosController.cs
@@ -0,0 +1,34 @@
+﻿using Charpter.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Charpter.Controllers
+{
+    [Produces("appliccation/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+
+        private readonly LivroRepositery? _livroRepositery;
+        public LivrosController(LivroRepositery livroRepositery)
+        {
+            _livroRepositery = livroRepositery;
+        }
+
+        [HttpGet]
+
+        public IActionResult ler()
+        {
+            try
+            {
+                return Ok( _livroRepositery.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+    }
+}
---
+++ Charpter/Charpter/Repositories/LivroRepositery.cs
@@ -0,0 +1,21 @@
+﻿using Charpter.Contexts;
+using Charpter.Models;
+
+namespace Charpter.Repositories
+{
+    public class LivroRepositery
+    {
+        private readonly Sqlcontext _context;
+        public LivroRepositery(Sqlcontext context) 
+        {
+                _context = context;
+
+        }
+        public List<Livro> Listar()
+        {
+            return _context.Livros.ToList();
+        }
+        
+
+    }
+}
